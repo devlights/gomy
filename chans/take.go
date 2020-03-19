9aaf216977945b28e1bdbe2ba7ecb4b1aad46656
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-03-19 12:08:58 +0900

    Add take.go
    - Take
    - TakeWhile

---
+++ chans/take.go
@@ -0,0 +1,51 @@
+package chans
+
+// Take -- 指定した個数分、入力用チャネルから値を取得するチャネルを返します。
+func Take(done <-chan struct{}, in <-chan interface{}, count int) <-chan interface{} {
+	out := make(chan interface{})
+
+	go func() {
+		defer close(out)
+
+		for i := 0; i < count; i++ {
+			select {
+			case <-done:
+				return
+			case out <- <-in:
+			}
+		}
+	}()
+
+	return out
+}
+
+// TakeWhile -- 入力用チャネルから取得した値が指定した値と同一である間、値を取得し続けるチャネルを返します。
+func TakeWhile(done <-chan struct{}, in <-chan interface{}, value interface{}) <-chan interface{} {
+	out := make(chan interface{})
+
+	go func(value interface{}) {
+		defer close(out)
+
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+
+				if v != value {
+					return
+				}
+
+				select {
+				case out <- v:
+				case <-done:
+				}
+			}
+		}
+	}(value)
+
+	return out
+}
