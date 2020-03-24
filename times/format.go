e2dda3f6f853c3f3f2b5610714e1cc7fbe78f65f
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-03-24 15:58:22 +0900

    Add format.go

---
+++ times/format.go
@@ -0,0 +1,20 @@
+package times
+
+import (
+	"time"
+)
+
+// YyyyMmdd -- 指定された time.Time を yyyy/MM/dd 形式にフォーマットして返します。
+func YyyyMmdd(t time.Time) string {
+	return t.Format("2006/1/2")
+}
+
+// YyyyMmddHHmmss -- 指定された time.Time を yyyy/MM/dd HH:mm:ss 形式にフォーマットして返します。
+func YyyyMmddHHmmss(t time.Time) string {
+	return t.Format("2006/1/2 15:04:05")
+}
+
+// YyyyMmddHHmmssWithMilliSec -- YyyyMmddHHmmss() の結果にミリ秒を付与したものを返します。
+func YyyyMmddHHmmssWithMilliSec(t time.Time) string {
+	return t.Format("2006/1/2 15:04:05.000")
+}
---
+++ times/format_test.go
@@ -0,0 +1,59 @@
+package times
+
+import (
+	"testing"
+	"time"
+)
+
+func TestTimeFormat(t *testing.T) {
+	type (
+		testin struct {
+			t  time.Time
+			fn func(t time.Time) string
+		}
+		testout struct {
+			r string
+		}
+		testcase struct {
+			in  testin
+			out testout
+		}
+	)
+
+	cases := []testcase{
+		{
+			in: testin{
+				t:  time.Date(2999, 12, 31, 11, 12, 13, 987654321, time.Local),
+				fn: YyyyMmdd,
+			},
+			out: testout{
+				r: "2999/12/31",
+			},
+		},
+		{
+			in: testin{
+				t:  time.Date(2999, 12, 31, 11, 12, 13, 987654321, time.Local),
+				fn: YyyyMmddHHmmss,
+			},
+			out: testout{
+				r: "2999/12/31 11:12:13",
+			},
+		},
+		{
+			in: testin{
+				t:  time.Date(2999, 12, 31, 11, 12, 13, 987654321, time.Local),
+				fn: YyyyMmddHHmmssWithMilliSec,
+			},
+			out: testout{
+				r: "2999/12/31 11:12:13.987",
+			},
+		},
+	}
+
+	for _, c := range cases {
+		result := c.in.fn(c.in.t)
+		if c.out.r != result {
+			t.Errorf("want: %v\tgot: %v", c.out.r, result)
+		}
+	}
+}
