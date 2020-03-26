246e2c1eb0e044d88274445a6f63d573f22be062
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-03-26 14:55:31 +0000

    Update fanout_test.go

--- chans/fanout_test.go
+++ chans/fanout_test.go
@@ -1,75 +1,85 @@
 package chans
 
 import (
-	"sort"
 	"testing"
 	"time"
-
-	"github.com/devlights/gomy/output"
 )
 
 func TestFanOut(t *testing.T) {
 	type (
-		result struct {
-			index int
-			value interface{}
+		testin struct {
+			numGoroutine int
+			input        []interface{}
+			interval     time.Duration
+		}
+		testout struct {
+			estimation time.Duration
+		}
+		testcase struct {
+			in  testin
+			out testout
 		}
 	)
 
-	var (
-		givenTime    = 1 * time.Second
-		numGoroutine = 2
-		items        = []interface{}{"hello", "world", "こんにちわ", "世界"}
-		results      = make([]*result, 0, 0)
-	)
-	var (
-		done  = make(chan struct{})
-		outCh = make(chan interface{})
-	)
-
-	defer close(done)
-
-	// 処理するのに t に指定された時間がかかる関数
-	fn := func(item interface{}, t time.Duration) {
-		<-time.After(t)
-		output.Stdoutl("[処理]", item)
+	cases := []testcase{
+		{
+			in: testin{
+				numGoroutine: 1,
+				input: []interface{}{
+					1, 2, 3, 4, 5, 6,
+				},
+				interval: 100 * time.Millisecond,
+			},
+			out: testout{
+				estimation: ((6/1 + 1) * 100) * time.Millisecond,
+			},
+		},
+		{
+			in: testin{
+				numGoroutine: 2,
+				input: []interface{}{
+					1, 2, 3, 4, 5, 6,
+				},
+				interval: 100 * time.Millisecond,
+			},
+			out: testout{
+				estimation: ((6/2 + 1) * 100) * time.Millisecond,
+			},
+		},
+		{
+			in: testin{
+				numGoroutine: 3,
+				input: []interface{}{
+					1, 2, 3, 4, 5, 6,
+				},
+				interval: 100 * time.Millisecond,
+			},
+			out: testout{
+				estimation: ((6/3 + 1) * 100) * time.Millisecond,
+			},
+		},
 	}
 
-	// パイプライン生成
-	forEachCh := ForEach(done, items...)
-	enumerateCh := Enumerate(done, forEachCh)
-	doneChList := FanOut(done, enumerateCh, numGoroutine, func(e interface{}) {
-		if v, ok := e.(*IterValue); ok {
-			fn(v.Value, givenTime)
-			outCh <- &result{
-				index: v.Index,
-				value: v.Value,
+	for caseIndex, c := range cases {
+		func(index int) {
+			done := make(chan struct{})
+			defer close(done)
+
+			start := time.Now()
+			<-WhenAll(FanOut(
+				done,
+				ForEach(done, c.in.input...),
+				c.in.numGoroutine,
+				func(v interface{}) {
+					<-time.After(c.in.interval)
+					// t.Logf("[test-%02d] %v", index, v)
+				})...)
+			elapsed := time.Since(start)
+
+			t.Logf("[estimation] %v\t[elapsed] %v", c.out.estimation, elapsed)
+			if c.out.estimation < elapsed {
+				t.Errorf("want: <= %v\tgot: %v", c.out.estimation, elapsed)
 			}
-		}
-	})
-
-	// 処理完了とともに出力用チャネルを閉じる
-	go func() {
-		defer close(outCh)
-		<-WhenAll(doneChList...)
-	}()
-
-	// 結果を吸い出し
-	for v := range outCh {
-		results = append(results, v.(*result))
-	}
-
-	// 正しい順序に並び替え
-	sort.Slice(results, func(i, j int) bool {
-		return results[i].index < results[j].index
-	})
-
-	for i := 0; i < len(items); i++ {
-		l := items[i]
-		r := results[i].value
-
-		if l != r {
-			t.Errorf("want: %v\tgot: %v", l, r)
-		}
+		}(caseIndex + 1)
 	}
 }
