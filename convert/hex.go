eb182cd5b008591919446c2869376fb5b03b811e
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-05-23 03:29:32 +0900

    Add convert.Hex2Bin()

--- convert/hex.go
+++ convert/hex.go
@@ -41,3 +41,39 @@ func Hex2Dec(val string, prefix string, length int) (string, error) {
 
 	return result, nil
 }
+
+// Hex2Bin -- 指定された16進数文字列を2進数文字列にします.
+//
+// - 空文字を指定した場合は空文字が返ります.
+//
+// - 変換に失敗した場合は err に値が設定されます.
+//
+// - prefixを指定した場合、変換後の文字列の先頭に付与します.
+//
+// - lengthを指定した場合、その長さに合うようにゼロパディングします. 0の場合はパディング無しで変換されます.
+//   (e.g. length=4 で 16進数 8 を指定した場合 0100 となります.)
+func Hex2Bin(val string, prefix string, length int) (string, error) {
+	if val == "" {
+		return "", nil
+	}
+
+	val = strings.ToLower(val)
+	if strings.HasPrefix(val, "0x") {
+		val = strings.Replace(val, "0x", "", 1)
+	}
+
+	num, err := strconv.ParseInt(val, 16, 32)
+	if err != nil {
+		return "", err
+	}
+
+	format := "%s%b"
+	result := fmt.Sprintf(format, prefix, num)
+
+	if length > 0 {
+		format = "%s" + "%0" + strconv.Itoa(length) + "b"
+		result = fmt.Sprintf(format, prefix, num)
+	}
+
+	return result, nil
+}
--- convert/hex_test.go
+++ convert/hex_test.go
@@ -44,3 +44,44 @@ func TestHex2Dec(t *testing.T) {
 		})
 	}
 }
+
+func TestHex2Bin(t *testing.T) {
+	type args struct {
+		val    string
+		prefix string
+		length int
+	}
+	tests := []struct {
+		name            string
+		args            args
+		want            string
+		errShouldRaised bool
+	}{
+		{name: "empty", args: args{val: "", prefix: "", length: 0}, want: "", errShouldRaised: false},
+		{name: "not decimal", args: args{val: "xyz", prefix: "", length: 0}, want: "", errShouldRaised: true},
+		{name: "8 to 1000", args: args{val: "8", prefix: "", length: 0}, want: "1000", errShouldRaised: false},
+		{name: "8 to 0b1000", args: args{val: "8", prefix: "0b", length: 0}, want: "0b1000", errShouldRaised: false},
+		{name: "0x70 to 0b1110000", args: args{val: "0x70", prefix: "0b", length: 0}, want: "0b1110000", errShouldRaised: false},
+		{name: "0x70 to 0b01110000", args: args{val: "0x70", prefix: "0b", length: 8}, want: "0b01110000", errShouldRaised: false},
+		{name: "0x70 to 0b000001110000", args: args{val: "0x70", prefix: "0b", length: 12}, want: "0b000001110000", errShouldRaised: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Hex2Bin(tt.args.val, tt.args.prefix, tt.args.length)
+
+			if got != tt.want {
+				t.Errorf("Hex2Bin() = %v, want %v", got, tt.want)
+			}
+
+			if tt.errShouldRaised {
+				if err == nil {
+					t.Errorf("Hex2Bin() should raise err but err is nil")
+				}
+			} else {
+				if err != nil {
+					t.Errorf("Hex2Bin() err = %v", err)
+				}
+			}
+		})
+	}
+}
