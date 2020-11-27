0061bf6b3622d1cf86f4ff7f7e8d05acacb3a473
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-11-27 07:33:36 +0000

    Add ctxs.ToDoneCh function
    Fixes devlights/gomy#41

---
+++ ctxs/donech.go
@@ -0,0 +1,22 @@
+package ctxs
+
+import "context"
+
+// ToDoneCh は、指定された context.Context から done チャネルを取り出してスライスで返します.
+//
+// 取得した done チャネルのスライスを chans.WhenAll() に指定すると全コンテキストが完了するまで
+// 待機することが出来ます。
+//
+// Example:
+//
+// 		<-chans.WhenAll(ctxs.ToDoneCh(contexts...)...) // contexts is []context.Context
+//
+func ToDoneCh(contexts ...context.Context) []<-chan struct{} {
+	dones := make([]<-chan struct{}, len(contexts))
+
+	for i, ctx := range contexts {
+		dones[i] = ctx.Done()
+	}
+
+	return dones
+}
