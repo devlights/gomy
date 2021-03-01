808a40510748b07b61db1b7a30f903f1547fbc04
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-03-01 06:37:00 +0000

    Add chans.Bridge tests

--- chans/bridge_test.go
+++ chans/bridge_test.go
@@ -1,1 +1,55 @@
 package chans_test
+
+import (
+	"context"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/devlights/gomy/chans"
+)
+
+func TestBridge(t *testing.T) {
+	cases := []struct {
+		name string
+		in   [][]interface{}
+		out  []interface{}
+	}{
+		{
+			name: "single",
+			in:   [][]interface{}{{1, 2, 3}},
+			out:  []interface{}{1, 2, 3},
+		},
+		{
+			name: "multi",
+			in:   [][]interface{}{{1, 2, 3}, {4, 5, 6}},
+			out:  []interface{}{1, 2, 3, 4, 5, 6},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			var (
+				ctx, cxl = context.WithTimeout(context.Background(), 50*time.Millisecond)
+			)
+			defer cxl()
+
+			chSeq := make(chan (<-chan interface{}))
+			go func() {
+				defer close(chSeq)
+				for _, v := range c.in {
+					chSeq <- chans.Generator(ctx.Done(), v...)
+				}
+			}()
+
+			var results []interface{}
+			for v := range chans.Bridge(ctx.Done(), chSeq) {
+				results = append(results, v)
+			}
+
+			if !reflect.DeepEqual(c.out, results) {
+				t.Errorf("[want] %v\t[got] %v", c.out, results)
+			}
+		})
+	}
+}
