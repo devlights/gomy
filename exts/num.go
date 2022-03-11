7b9f51eae2a5e0f11dc43f212535ddc29ee4e103
devlights <gsf.zero1@devlights.onmicrosoft.com>
2022-03-11 04:57:52 +0000

    Add comments

--- exts/num.go
+++ exts/num.go
@@ -23,12 +23,16 @@ func (me Num) Times(fn func(i int)) {
 
 // Upto は、自身の値から to の値までの回数ループし、ループの度に fn を実行します。
 // fn の引数に指定されるのは index となります。
+// Downto との違いは、Uptoはインデックスが正の方向に上がっていく(i++)のに対し、Downto は負の方向へ下がっていきます。(i--)
 func (me Num) Upto(to int, fn func(i int)) {
 	for i := int(me); i <= to; i++ {
 		fn(i)
 	}
 }
 
+// Downto は、自身の値から to 値までの回数ループし、ループの度に fn を実行します。
+// fn の引数に指定されるのは index となります。
+// Upto との違いは、Uptoはインデックスが正の方向に上がっていく(i++)のに対し、Downto は負の方向へ下がっていきます。(i--)
 func (me Num) Downto(to int, fn func(i int)) {
 	for i := int(me); i >= to; i-- {
 		fn(i)
