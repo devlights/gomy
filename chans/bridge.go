f1fc63518e04ca14e9aaf8d79d21a6107d6cba15
devlights <gsf.zero1@devlights.onmicrosoft.com>
2022-09-06 00:56:42 +0000

    Add chans.BridgeContext

--- chans/bridge.go
+++ chans/bridge.go
@@ -1,5 +1,12 @@
 package chans
 
+import "context"
+
+// BridgeContext-- Bridge の context.Context 版です。
+func BridgeContext[T any](ctx context.Context, chanCh <-chan <-chan T) <-chan T {
+	return Bridge(ctx.Done(), chanCh)
+}
+
 // Bridge -- 指定されたチャネルのシーケンスを順に消費していく単一のチャネルを返します。
 func Bridge[T any](done <-chan struct{}, chanCh <-chan <-chan T) <-chan T {
 	out := make(chan T)
--- chans/bridge_test.go
+++ chans/bridge_test.go
@@ -9,6 +9,51 @@ import (
 	"github.com/devlights/gomy/chans"
 )
 
+func TestBridgeContext(t *testing.T) {
+	cases := []struct {
+		name string
+		in   [][]interface{}
+		out  []interface{}
+	}{
+		{
+			name: "single",
+			in:   [][]interface{}{{1, 2, 3}},
+			out:  []interface{}{1, 2, 3},
+		},
+		{
+			name: "multi",
+			in:   [][]interface{}{{1, 2, 3}, {4, 5, 6}},
+			out:  []interface{}{1, 2, 3, 4, 5, 6},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			var (
+				ctx, cxl = context.WithTimeout(context.Background(), 50*time.Millisecond)
+			)
+			defer cxl()
+
+			chSeq := make(chan (<-chan interface{}))
+			go func() {
+				defer close(chSeq)
+				for _, v := range c.in {
+					chSeq <- chans.Generator(ctx.Done(), v...)
+				}
+			}()
+
+			var results []interface{}
+			for v := range chans.BridgeContext(ctx, chSeq) {
+				results = append(results, v)
+			}
+
+			if !reflect.DeepEqual(c.out, results) {
+				t.Errorf("[want] %v\t[got] %v", c.out, results)
+			}
+		})
+	}
+}
+
 func TestBridge(t *testing.T) {
 	cases := []struct {
 		name string
