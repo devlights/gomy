2a98d8cc5255bc44acf11c8b6bd414cfd009e8b2
devlights <gsf.zero1@devlights.onmicrosoft.com>
2022-09-06 01:41:57 +0000

    Add chans.ConcatContext

--- chans/concat.go
+++ chans/concat.go
@@ -1,22 +1,29 @@
 package chans
 
-// Concat -- 指定されたチャネルのシーケンスを順に消費していく単一のチャネルを返します。
-func Concat[T any](done <-chan struct{}, chList ...<-chan T) <-chan T {
-	if len(chList) == 0 {
+import "context"
+
+// ConcatContext は、 Concat の context.Context 版です.
+func ConcatContext[T any](ctx context.Context, channels ...<-chan T) <-chan T {
+	return Concat(ctx.Done(), channels...)
+}
+
+// Concat は、指定されたチャネルのシーケンスを順に消費していく単一のチャネルを返します.
+func Concat[T any](done <-chan struct{}, channels ...<-chan T) <-chan T {
+	if len(channels) == 0 {
 		c := make(chan T)
 		close(c)
 
 		return c
 	}
 
-	chSeq := make(chan (<-chan T), len(chList))
+	sequences := make(chan (<-chan T), len(channels))
 	func() {
-		defer close(chSeq)
+		defer close(sequences)
 
-		for _, c := range chList {
-			chSeq <- c
+		for _, c := range channels {
+			sequences <- c
 		}
 	}()
 
-	return Bridge(done, chSeq)
+	return Bridge(done, sequences)
 }
