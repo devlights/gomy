78b3b092e88450cb3a6a1a34f40f275cf002570e
devlights <gsf.zero1@devlights.onmicrosoft.com>
2022-09-06 01:41:50 +0000

    Add chans.ChunkContext

--- chans/chunk.go
+++ chans/chunk.go
@@ -1,5 +1,12 @@
 package chans
 
+import "context"
+
+// ChunkContext は、Chunk の context.Context 版です.
+func ChunkContext[T any](ctx context.Context, in <-chan T, count int) <-chan []T {
+	return Chunk(ctx.Done(), in, count)
+}
+
 // Chunkは、入力を指定した件数分に束ねてデータを返すチャネルを生成します.
 //
 // Buffer関数のエイリアスです。
