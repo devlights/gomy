110488849e65c94f97420f893bc1f37d03f21113
devlights <gsf.zero1@devlights.onmicrosoft.com>
2022-09-07 05:50:12 +0000

    Add chans.OrDoneContext

--- chans/ordone.go
+++ chans/ordone.go
@@ -1,5 +1,12 @@
 package chans
 
+import "context"
+
+// OrDoneContext は、OrDone の context.Context 版です.
+func OrDoneContext[T any](ctx context.Context, in <-chan T) <-chan T {
+	return OrDone(ctx.Done(), in)
+}
+
 // OrDone -- 指定された終了チャネルと入力用チャネルのどちらかが閉じたら閉じるチャネルを返します。
 func OrDone[T any](done <-chan struct{}, in <-chan T) <-chan T {
 	out := make(chan T)
