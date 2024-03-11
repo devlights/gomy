c50a962fe1a02a36c30f28204f913595d7e4aea5
devlights <gsf.zero1@devlights.onmicrosoft.com>
2024-03-11 09:28:18 +0000

    Update

--- zeromemcpy/s2b.go
+++ zeromemcpy/s2b.go
@@ -1,7 +1,6 @@
 package zeromemcpy
 
 import (
-	"reflect"
 	"unsafe"
 )
 
@@ -10,5 +9,5 @@ import (
 // REREFENCES
 //   - https://stackoverflow.com/questions/59209493/how-to-use-unsafe-get-a-byte-slice-from-a-string-without-memory-copy
 func S2B(val string) []byte {
-	return unsafe.Slice((*byte)(unsafe.Pointer((*reflect.StringHeader)(unsafe.Pointer(&val)).Data)), len(val))
+	return unsafe.Slice(unsafe.StringData(val), len(val))
 }
